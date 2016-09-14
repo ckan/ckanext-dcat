9d1d437d96922fa0b08e6a6a036ce551c7ce9702
amercader <amercadero@gmail.com>
2016-09-14 14:41:21 +0100

    Install html5lib in the travis setup

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -42,6 +42,8 @@ paster harvester initdb -c ../ckan/test-core.ini
 cd -
 
 echo "Installing ckanext-dcat and its requirements..."
+# Pin this as newer versions installed by RDFLib give setuptools troubles
+pip install "html5lib==0.9999999"
 pip install -r requirements.txt --allow-all-external
 pip install -r dev-requirements.txt --allow-all-external
 python setup.py develop
