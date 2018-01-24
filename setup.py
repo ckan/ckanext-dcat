407234d83f38162714dcaa120ed18a472395a968
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2018-01-24 09:27:56 +0100

    [#107] Remove ckanext.dcat as a namespace package

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url='https://github.com/okfn/ckanext-dcat',
     license='AGPL',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    namespace_packages=['ckanext', 'ckanext.dcat'],
+    namespace_packages=['ckanext'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
