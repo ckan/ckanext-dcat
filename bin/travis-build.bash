a9f290143e25c5017edc1e991d57924219b43fad
amercader <amercadero@gmail.com>
2018-11-08 10:31:55 +0100

    Fix travis build on 2.7

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -37,6 +37,12 @@ pip install -r requirements.txt
 pip install -r dev-requirements.txt
 cd -
 
+
+if [ $CKANVERSION == '2.7' ]
+then
+    pip install setuptools==36.1
+fi
+
 echo "Setting up Solr..."
 printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
