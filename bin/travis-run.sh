e322f896f76f02012e8ee93155b122e1cda467f4
amercader <amercadero@gmail.com>
2020-02-04 11:41:31 +0100

    Fix pytest command

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -2,7 +2,7 @@
 
 if [ $CKAN_MINOR_VERSION >= 9 ]
 then
-    pytests --ckan-ini=subdir/test.ini --cov=ckanext.dcat ckanext/dcat/tests
+    pytest --ckan-ini=subdir/test.ini --cov=ckanext.dcat ckanext/dcat/tests
 else
     nosetests --ckan --nologcapture --with-pylons=subdir/test-nose.ini --with-coverage --cover-package=ckanext.dcat --cover-inclusive --cover-erase --cover-tests ckanext/dcat/tests/nose
 fi
