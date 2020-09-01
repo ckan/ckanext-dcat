6297f4da34a3ccfd5e5f7b9b62f774de296d7d9a
amercader <amercadero@gmail.com>
2020-09-01 12:35:40 +0200

    Fix unicode ref

--- ckanext/dcat/profiles.py
+++ ckanext/dcat/profiles.py
@@ -459,10 +459,7 @@ class RDFProfile(object):
             if isinstance(obj, BNode) and self._object(obj, RDF.type) == DCT.RightsStatement:
                 result = self._object_value(obj, RDFS.label)
             elif isinstance(obj, Literal):
-                if six.PY2:
-                    result = unicode(obj)
-                else:
-                    result = str(obj)
+                result = six.text_type(obj)
         return result
 
     def _distribution_format(self, distribution, normalize_ckan_format=True):
