e0f15f5a83c614b54f4d7e625236720e14073664
amercader <amercadero@gmail.com>
2024-05-21 11:56:52 +0200

    [#56] e2e test DCAT -> CKAN

--- ckanext/dcat/tests/test_scheming_support.py
+++ ckanext/dcat/tests/test_scheming_support.py
@@ -5,7 +5,7 @@ from rdflib.namespace import RDF
 from ckan.tests.helpers import call_action
 
 from ckanext.dcat import utils
-from ckanext.dcat.processors import RDFSerializer
+from ckanext.dcat.processors import RDFSerializer, RDFParser
 from ckanext.dcat.profiles import (
     DCAT,
     DCT,
@@ -22,7 +22,7 @@ from ckanext.dcat.profiles import (
     GEOJSON_IMT,
     DISTRIBUTION_LICENSE_FALLBACK_CONFIG,
 )
-from ckanext.dcat.tests.utils import BaseSerializeTest
+from ckanext.dcat.tests.utils import BaseSerializeTest, BaseParseTest
 
 
 @pytest.mark.usefixtures("with_plugins", "clean_db")
@@ -31,7 +31,10 @@ from ckanext.dcat.tests.utils import BaseSerializeTest
     "scheming.dataset_schemas", "ckanext.dcat.schemas:dcat_ap_2.1.yaml"
 )
 @pytest.mark.ckan_config("scheming.presets", "ckanext.scheming:presets.json")
-class TestSchemingSupport(BaseSerializeTest):
+@pytest.mark.ckan_config(
+    "ckanext.dcat.rdf.profiles", "euro_dcat_ap_2 euro_dcat_ap_scheming"
+)
+class TestSchemingSerializeSupport(BaseSerializeTest):
     def test_e2e_ckan_to_dcat(self):
         """
         Create a dataset using the scheming schema, check that fields
@@ -81,7 +84,7 @@ class TestSchemingSupport(BaseSerializeTest):
         assert dataset["conforms_to"][0] == "Standard 1"
         assert dataset["contact"][0]["name"] == "Contact 1"
 
-        s = RDFSerializer(profiles=["euro_dcat_ap_2"])
+        s = RDFSerializer()
         g = s.g
 
         dataset_ref = s.graph_from_dataset(dataset)
@@ -163,3 +166,74 @@ class TestSchemingSupport(BaseSerializeTest):
             endpoint_urls
             == dataset_dict["resources"][0]["access_services"][0]["endpoint_url"]
         )
+
+
+@pytest.mark.usefixtures("with_plugins", "clean_db")
+@pytest.mark.ckan_config("ckan.plugins", "dcat scheming_datasets")
+@pytest.mark.ckan_config(
+    "scheming.dataset_schemas", "ckanext.dcat.schemas:dcat_ap_2.1.yaml"
+)
+@pytest.mark.ckan_config("scheming.presets", "ckanext.scheming:presets.json")
+@pytest.mark.ckan_config(
+    "ckanext.dcat.rdf.profiles", "euro_dcat_ap_2 euro_dcat_ap_scheming"
+)
+class TestSchemingParseSupport(BaseParseTest):
+    def test_e2e_dcat_to_ckan(self):
+        """
+        Parse a DCAT RDF graph into a CKAN dataset dict, create a dataset with package_create
+        and check that all expected fields are there
+        """
+        contents = self._get_file_contents("dataset.rdf")
+
+        p = RDFParser()
+
+        p.parse(contents)
+
+        datasets = [d for d in p.datasets()]
+
+        assert len(datasets) == 1
+
+        dataset_dict = datasets[0]
+
+        dataset_dict["name"] = "test-dcat-1"
+        dataset = call_action("package_create", **dataset_dict)
+
+        # Core fields
+
+        assert dataset["title"] == "Zimbabwe Regional Geochemical Survey."
+        assert (
+            dataset["notes"]
+            == "During the period 1982-86 a team of geologists from the British Geological Survey ..."
+        )
+        assert dataset["url"] == "http://dataset.info.org"
+        assert dataset["version"] == "2.3"
+        assert dataset["license_id"] == "cc-nc"
+        assert sorted([t["name"] for t in dataset["tags"]]) == [
+            "exploration",
+            "geochemistry",
+            "geology",
+        ]
+
+        # Standard fields
+        assert dataset["version_notes"] == "New schema added"
+
+        # List fields
+        assert dataset["conforms_to"] == ["Standard 1", "Standard 2"]
+
+        # Repeating subfields
+
+        assert dataset["contact"][0]["name"] == "Point of Contact"
+        assert dataset["contact"][0]["email"] == "contact@some.org"
+
+        resource = dataset["resources"][0]
+        # Resources: standard fields
+        assert resource["rights"] == "Some statement about rights"
+
+        # Resources: list fields
+        assert sorted(resource["language"]) == ["ca", "en", "es"]
+
+        # Resources: repeating subfields
+        assert resource["access_services"][0]["title"] == "Sparql-end Point"
+        assert resource["access_services"][0]["endpoint_url"] == [
+            "http://publications.europa.eu/webapi/rdf/sparql"
+        ]
