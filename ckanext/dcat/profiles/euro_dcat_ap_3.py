d4803138163e88949f46d6bbecc3b7d00436b721
amercader <amercadero@gmail.com>
2024-08-22 13:16:52 +0200

    Distribution byte size should be nonNegativeInteger

--- ckanext/dcat/profiles/euro_dcat_ap_3.py
+++ ckanext/dcat/profiles/euro_dcat_ap_3.py
@@ -1,4 +1,6 @@
-from ckanext.dcat.profiles import EuropeanDCATAP2Profile
+from rdflib import Literal
+
+from ckanext.dcat.profiles import EuropeanDCATAP2Profile, DCAT, XSD
 
 
 class EuropeanDCATAP3Profile(EuropeanDCATAP2Profile):
@@ -6,3 +8,20 @@ class EuropeanDCATAP3Profile(EuropeanDCATAP2Profile):
     An RDF profile based on the DCAT-AP 3 for data portals in Europe
     """
 
+    def graph_from_dataset(self, dataset_dict, dataset_ref):
+
+        # Call the DCAT AP 2 method
+        super().graph_from_dataset(dataset_dict, dataset_ref)
+
+        # byteSize decimal -> nonNegativeInteger
+        for subject, predicate, object in self.g.triples((None, DCAT.byteSize, None)):
+            if object and object.datatype == XSD.decimal:
+                self.g.remove((subject, predicate, object))
+
+                self.g.add(
+                    (
+                        subject,
+                        predicate,
+                        Literal(int(object), datatype=XSD.nonNegativeInteger),
+                    )
+                )
