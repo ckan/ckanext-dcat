8760d6d6d37e40f55385e4c6737f95742fcb6848
amercader <amercadero@gmail.com>
2024-09-26 13:49:38 +0200

    Implement PEP 420 namespace packages

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
     url='https://github.com/okfn/ckanext-dcat',
     license='AGPL',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    namespace_packages=['ckanext'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
