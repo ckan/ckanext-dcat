d0c9bfed0208214ac77d411374557d3413c13cb1
amercader <amercadero@gmail.com>
2025-02-19 15:36:06 +0100

    Add validation test for recordset
    Currently failing because SCHEMA.DateTime is not an expected type??

--- ckanext/dcat/tests/profiles/croissant/test_validate.py
+++ ckanext/dcat/tests/profiles/croissant/test_validate.py
@@ -1,5 +1,6 @@
 import json
 import sys
+from unittest import mock
 
 try:
     import mlcroissant as mlc
@@ -15,7 +16,7 @@ from ckanext.dcat.tests.utils import get_file_contents
 
 
 @pytest.mark.skipif(
-    sys.version_info < (3, 10), reason="croissant is not available in py<3.10"
+    sys.version_info < (3, 10), reason="mlcroissant is not available in py<3.10"
 )
 def test_valid_output():
 
@@ -37,3 +38,43 @@ def test_valid_output():
         mlc.Dataset(croissant_dict)
     except mlc.ValidationError as exception:
         raise
+
+
+@pytest.mark.skipif(
+    sys.version_info < (3, 10), reason="mlcroissant is not available in py<3.10"
+)
+def test_valid_output_with_recordset():
+
+    dataset_dict = json.loads(
+        get_file_contents("ckan/ckan_full_dataset_croissant.json")
+    )
+
+    resource_id = dataset_dict["resources"][0]["id"]
+
+    def mock_datastore_info(context, data_dict):
+        return {
+            "meta": {"id": resource_id, "count": 10, "table_type": "BASE TABLE"},
+            "fields": [
+                {"id": "name", "type": "text", "schema": {"is_index": True}},
+                {"id": "age", "type": "int", "schema": {"is_index": False}},
+                {"id": "timestamp", "type": "timestamp", "schema": {"is_index": False}},
+            ],
+        }
+
+    with mock.patch("ckanext.dcat.profiles.croissant.get_action") as mock_get_action:
+        mock_get_action.return_value = mock_datastore_info
+
+        s = RDFSerializer(profiles=["croissant"])
+
+        s.graph_from_dataset(dataset_dict)
+
+        croissant_dict = json.loads(
+            s.g.serialize(format="json-ld", auto_compact=True, context=JSONLD_CONTEXT)
+        )
+        with open("graph.jsonld", "w") as f:
+            f.write(json.dumps(croissant_dict))
+
+        try:
+            mlc.Dataset(croissant_dict)
+        except mlc.ValidationError as exception:
+            raise
